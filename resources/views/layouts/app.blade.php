86fad3575964c5adc1a813bf3f5460156cdc46a3
Jjx51 <jjx51@outlook.com>
2017-12-07 23:14:39 -0600

    Barra de navegacion configurada para los 3 tipos de usuarios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        Ciclo Envios
                     </a>
                 </div>
 
@@ -37,14 +37,45 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
+                        <!-- Authentication Links -->
+                        @if (Auth::check())
+                            @if(Auth::user()->rol=='user')
+                                <li><a href="#">Hacer un pedido</a></li>
+                                <li><a href="#">Ver mis pedidos</a></li>
+                            @endif
+                            @if(Auth::user()->rol=='cyclist')
+                                <li><a href="#">Ver pedidos asignados</a></li>
+                            @endif
+
+                            @if(Auth::user()->rol=='admin')
+                                <li><a href="#">Todos los pedidos</a></li>
+                                <li><a href="#">Hacer un pedido</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle hidden-sm" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        Administración de usuarios <span class="caret"></span>
+                                    </a>
+                                    <a href="#" class="dropdown-toggle visible-sm" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        Usuarios <span class="caret"></span>
+                                    </a>
+
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li>
+                                            <a href="#">Crear nuevo usuario</a>
+                                            <a href="#">Usuarios existentes</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            @endif
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
+                        
+
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">Iniciar sesión</a></li>
+                            <li><a href="{{ route('register') }}">Registrarse</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -56,7 +87,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Cerrar sesión
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
