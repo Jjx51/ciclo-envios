372bb0ff3d9d870cb1b482956fd21946597aa2b3
Jjx51 <jjx51@outlook.com>
2017-12-07 22:04:18 -0600

    Creacion del Factory para Users

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,8 +17,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->name,
+        'username'=>$faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'rol'=>$faker->randomElement(['user','cyclist','admin'])
     ];
 });
